ee4a6b0c1211a377e7a5a5b5ecb666bbe43e0a93
Thierry1 <floveandroid@sina.com>
2018-11-13 19:09:21 +0800

    添加了新的副本文件

---
+++ MyTextView2 - 副本.java
@@ -0,0 +1,42 @@
+package com.example.custom_view.eventdispatch;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * 爱生活，爱代码
+ * 创建于：2018/11/13 14:30
+ * 作 者：T 啊嘎哈哈哈哈哈哈嘎嘎嘎嘎
+ * 微信：704003376
+ */
+public class MyTextView extends TextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.e("asdhds", "View-----dispatchTouchEvent---super");
+        return super.dispatchTouchEvent(event);
+    }
+
+
+//测试出了BUG 这里面做了修改，啦啦啦
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.e("asdhds", "View-----onTouchEvent---super");
+        return super.onTouchEvent(event);
+    }
+}
